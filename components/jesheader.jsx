d7a5d655842717940eb8083e6dd4cc23e2bfc90c
tawakalmit <tawakal.mit@gmail.com>
2022-08-27 16:54:34 +0700

    add jesheader component

---
+++ components/jesheader.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function Jesheader() {
+  return (
+    <div>
+    <Head>
+      <title>Jes Petshop</title>
+      <link rel='icon' href='/jes_icon.png' />
+    </Head>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Jesheader from "../components/jesheader"
+
 export default function Home() {
   return (
+    <>
+    <Jesheader />
     <div className='w-full h-screen flex justify-center items-center'>
       <h1 className='text-[#1abc9c] text-7xl font-semibold'>Next JS Initialized</h1>
     </div>
+    </>
   )
 }
