e49513933161c38bb487f2174275ddbf459d9505
tawakalmit <tawakal.mit@gmail.com>
2022-08-27 16:48:36 +0700

    nextjs initialized

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <h1 className='text-[#1abc9c] text-7xl font-semibold'>Next JS Initialized</h1>
+    </div>
+  )
+}
