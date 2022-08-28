f1c32433121d160d842a3da95bc6fd75ab1e2cf0
tawakalmit <tawakal.mit@gmail.com>
2022-08-28 20:22:12 +0700

    fix home button and add to cart

--- components/produk_card.jsx
+++ components/produk_card.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
+import { MdOutlineAddShoppingCart } from 'react-icons/md'
 
 export default function Produk_card() {
   return (
-    <div className='w-36 h-fit mb-5 cursor-pointer bg-[#3498db] rounded-2xl  hover:opacity-70'>
+    <div className='w-36 h-fit mb-5 cursor-pointer bg-white rounded-2xl  hover:opacity-70'>
         <Image src="/1.png" width={300} height={300} className="rounded-t-2xl" />
         <div className="pb-2">
-            <p className="text-sm text-white text-center">Royal Canin Kitten</p>
-            <p className="text-sm text-white text-center">Rp 200.000</p>
+            <p className="text-sm text-center">Royal Canin Kitten</p>
+            <p className="text-sm text-center">Rp 200.000</p>
         </div>
+        <div className="flex justify-center items-center p-3 bg-[#3498db] rounded-b-2xl text-white text-sm"><MdOutlineAddShoppingCart />Keranjang</div>
     </div>
   )
 }
