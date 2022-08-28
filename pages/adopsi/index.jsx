23a5533d7ad6a3f7bbbf06c4f556472ad037af31
tawakalmit <tawakal.mit@gmail.com>
2022-08-28 17:55:08 +0700

    tag head adopsi

--- pages/adopsi/index.jsx
+++ pages/adopsi/index.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../../components/navbar"
 import { Image } from "@mantine/core"
 import Adopsi_card from "../../components/adopsi_card"
+import Jesheader from "../../components/jesheader"
 
 export default function Adopsi() {
   return (
     <>
+    <Jesheader judul="Jes Petshop | Adopsi" />
     <Navbar menu_kiri={<Image src="/logo_copy.png" width={70} />} page="Adopsi" />
     <div className="bg-[url('/bg.png')] h-full min-h-screen pt-10 w-full">
         <div className="w-10/12 flex flex-wrap mx-auto justify-around">
